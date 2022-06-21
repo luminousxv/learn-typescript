2f5a4fa1419469d4d680cd00dd58dd45f4e97014
luminousxv <drgvyhn@gmail.com>
2022-06-21 15:41:45 +0900

    기본 ts 문법 실습, tsc 사용법, tsconfig.js 작성법

---
+++ why-ts/sample.js
@@ -0,0 +1,17 @@
+// function sum(a, b) {
+//     return a+b;
+// }
+// // sum(10, '20');
+// sum()
+
+// @ts-check
+/**
+ * 
+ * @param {number} a 
+ * @param {number} b 
+ */
+
+function sum1 (a, b) {
+    return a + b;
+}
+sum1(10, 20);
