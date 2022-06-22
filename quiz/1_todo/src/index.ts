e8374d2ae46eef35a2441896185a706e0797358c
luminousxv <drgvyhn@gmail.com>
2022-06-22 15:01:49 +0900

    js -> ts 변환 실습

--- quiz/1_todo/src/index.ts
+++ quiz/1_todo/src/index.ts
@@ -1,8 +1,22 @@
-let todoItems: { id: number; title: string; done: boolean }[];
+// type Todo = {
+//   id: number;
+//   title: string;
+//   done: boolean;
+// };
+
+interface Todo {
+  id: number;
+  title: string;
+  done: boolean;
+};
+
+let todoItems: Todo[]; //object[]
+
+
 
 // api
-function fetchTodoItems(): { id: number; title: string; done: boolean }[] {
-  const todos: { id: number; title: string; done: boolean }[] = [
+function fetchTodoItems(): Todo[]/*object[]*/ {
+  const todos: Todo[] = [
     { id: 1, title: "안녕", done: false },
     { id: 2, title: "타입", done: false },
     { id: 3, title: "스크립트", done: false },
@@ -16,15 +30,15 @@ function fetchTodos(): object[] {
   return todos;
 }
 
-function addTodo(todo: {id: number, title: string, done: boolean}):void {
+function addTodo(todo: Todo/*object*/): void {
   todoItems.push(todo);
 }
 
-function deleteTodo(index: number):void {
+function deleteTodo(index: number): void {
   todoItems.splice(index, 1);
 }
 
-function completeTodo(index: number, todo: {id: number, title: string, done: boolean}):void {
+function completeTodo(index: number, todo: Todo/*object*/): void {
   todo.done = true;
   todoItems.splice(index, 1, todo);
 }
@@ -40,7 +54,7 @@ function showCompleted(): object[] {
 
 // TODO: 아래 함수의 내용을 채워보세요. 아래 함수는 `addTodo()` 함수를 이용하여 2개의 새 할 일을 추가하는 함수입니다.
 function addTwoTodoItems(): void {
-  const new_todo: {id: number, title: string, done: boolean}[] = [
+  const new_todo: Todo[] = [
     {id: 4, title: '많이', done: false},
     {id: 5, title: '어렵다', done: false}
   ]
@@ -49,7 +63,7 @@ function addTwoTodoItems(): void {
 }
 
 // NOTE: 유틸 함수
-function log() {
+function log(): void {
   console.log(todoItems);
 }
 
