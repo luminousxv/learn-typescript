9bd9aee0a7b26dd481cd5a7c08242c7d979a006d
luminousxv <drgvyhn@gmail.com>
2022-06-25 17:57:20 +0900

    type, interface 타입 선언 실습

---
+++ class-note/4_type-aliases.ts
@@ -0,0 +1,19 @@
+// interface Person {
+//     name: string;
+//     age: number
+// }
+
+type Person = {
+    name: string;
+    age: number
+}
+
+let seho:  Person = {
+    name: '세호',
+    age: 30
+}
+
+type MyString =  string;
+let str: MyString = 'hello';
+
+type Todo = {id : string; title :string; done: boolean};
