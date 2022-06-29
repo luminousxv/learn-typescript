5b587bab92f060797b36e846bcf03e2a1c3484a7
luminousxv <drgvyhn@gmail.com>
2022-06-29 15:58:31 +0900

    제네릭 실전 예제 실습

---
+++ class-note/dropdown-generic.ts
@@ -0,0 +1,48 @@
+interface DropdownItem<T> {
+  value: T;
+  selected: boolean;
+}
+
+// interface Email {
+//   value: string;
+//   selected: boolean;
+// }
+
+const emails: DropdownItem<string>[] = [
+  { value: "naver.com", selected: true },
+  { value: "gmail.com", selected: false },
+  { value: "hanmail.net", selected: false },
+];
+
+// interface ProductNumber {
+//   value: number;
+//   selected: boolean;
+// }
+
+const numberOfProducts: DropdownItem<number>[] = [
+  { value: 1, selected: true },
+  { value: 2, selected: false },
+  { value: 3, selected: false },
+];
+
+function createDropdownItem<T extends {toString: Function}>(item: DropdownItem<T>):HTMLOptionElement {
+  const option = document.createElement("option");
+  option.value = item.value.toString();
+  option.innerText = item.value.toString();
+  option.selected = item.selected;
+  return option;
+}
+
+// NOTE: 이메일 드롭 다운 아이템 추가
+emails.forEach(function (email) {
+  const item = createDropdownItem<string>(email);
+  const selectTag = document.querySelector('#email-dropdown');
+  selectTag?.appendChild(item);
+});
+
+//갯수 드롭 다운 아이템 
+numberOfProducts.forEach(function (products){
+  const item = createDropdownItem<number>(products);
+  const selectTag = document.querySelector('#product-dropdown');
+  selectTag?.appendChild(item);
+})
