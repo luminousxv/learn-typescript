af6476e0123b1c87c9c747b088281a0c42c30d05
luminousxv <drgvyhn@gmail.com>
2022-07-05 14:49:36 +0900

    전화번호부 실습에서 인터페이스, 이넘 모듈화

---
+++ quiz/2_address-book/src/types/types.ts
@@ -0,0 +1,17 @@
+export interface PhoneNumberDictionary {
+  [phones: string]: {
+    num: number;
+  };
+}
+
+ export interface Contact {
+  name: string;
+  address: string;
+  phones: PhoneNumberDictionary;
+}
+
+export enum PhoneType {
+  Home = "home",
+  Office = "office",
+  Studio = "studio",
+}
