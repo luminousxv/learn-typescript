7f80723614f7cb2e6b3d831012905cd98e3c9399
luminousxv <drgvyhn@gmail.com>
2022-07-17 13:47:48 +0900

    포스트맨 Covid-19 API 리턴값 타입 정의 단순화 중복되는 코드 수정

--- project/src/types/type.ts
+++ project/src/types/type.ts
@@ -1,34 +1,25 @@
 export interface Summary {
-  data: {
-    Global: Global;
-    Countries: Countries[];
-  };
+  Global: Global;
+  Countries: Countries[];
 }
 
-interface Global extends DeathRecovered {
+interface Global {
   NewConfirmed: number;
   TotalConfirmed: number;
-}
-
-interface Countries extends Global {
-  Country: string;
-  CountryCode: string;
-  Slug: string;
-  Date: number;
-}
-
-interface DeathRecovered {
   NewDeaths: number;
   TotalDeaths: number;
   NewRecovered: number;
   TotalRecovered: number;
 }
 
-export interface LiveCountryInfo {
-  data: Info[];
+export interface Countries extends Global {
+  Country: string;
+  CountryCode: string;
+  Slug: string;
+  Date: number;
 }
 
-interface Info {
+export interface LiveCountryInfo {
   Country: string;
   CountryCode: string;
   Cases: number;
